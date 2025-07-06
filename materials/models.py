1435f74e14b57cf6ac77ffd3d331b2b2e26bd3ba
Ivan_K <ikalinov1989yandex.ru>
2025-07-06 17:42:48 +0400

    Add models

--- materials/models.py
+++ materials/models.py
@@ -5,8 +5,6 @@ from django.db.models import ForeignKey, SET_NULL
 class Course(models.Model):
     title = models.CharField(
         max_length=100,
-        blank=True,
-        null=True,
         verbose_name="Название",
         help_text="Введите название",
     )
---
+++ materials/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from materials.views import CourseViewSet, LessonCreateAPIView, LessonListAPIVView, LessonRetrieveAPIView, LessonUpdateAPIView, LessonDestroyAPIView
+from materials.apps import MaterialsConfig
+
+app_name = MaterialsConfig.name
+
+router = SimpleRouter()
+router.register("", CourseViewSet)
+
+urlpatterns = [
+    path('lesson/', LessonListAPIVView.as_view(), name='lesson_list'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_retrieve'),
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lesson/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('lesson/<int:pk>/delete/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
+]
+
+urlpatterns += router.urls
