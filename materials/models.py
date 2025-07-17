b9543806cb3e6eeb644b441f00e7e931f31917e0
ivan_kalinov <ikalinov1989@yandex.ru>
2025-07-17 17:09:49 +0400

    Add filters

--- materials/models.py
+++ materials/models.py
@@ -3,7 +3,8 @@ from django.db.models import ForeignKey, SET_NULL
 
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+from config import settings
+
 
 class Course(models.Model):
     title = models.CharField(
@@ -26,7 +27,7 @@ class Course(models.Model):
         verbose_name="Описание",
         help_text="Введите описание",
     )
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, verbose_name='Владелец')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True, verbose_name='Владелец')
 
     class Meta:
         verbose_name = "Курс"
@@ -72,7 +73,7 @@ class Lesson(models.Model):
         verbose_name="Ссылка на видео",
         help_text="Загрузите ссылку на видео",
     )
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, verbose_name='Владелец')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True, verbose_name='Владелец')
 
     class Meta:
         verbose_name = "Урок"
